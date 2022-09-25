b79b561e6a6cff27c8ccf8507968857f00bb3404
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2022-09-25 19:38:44 +0600

    25.09.2022

--- Django/ToDo/home/views.py
+++ Django/ToDo/home/views.py
@@ -6,21 +6,16 @@ from django.shortcuts import render
 
 
 def home(request):
-    title = request.POST["title"]
-    description = request.POST["description"]
-
+    # title = request.POST["title"]
+    # description = request.POST["description"]
     connection = psycopg2.connect(user="postgres",
                                   password="Solo2005",
                                   host="127.0.0.1",
                                   port="5432",
                                   database="postgres")
     cursor = connection.cursor()
-    cursor.execute("""SELECT id, title, description FROM public."ToDoTasks";""")
-    data_sql = cursor.fetchall()
-    data = []
-    for i in data_sql:
-        data.append({"title": i[1], "description": i[2]})
-    if title in request.POST and title is not None and description is not None:
+
+    if request.method == "POST":
         title = request.POST["title"]
         description = request.POST["description"]
 
@@ -30,15 +25,13 @@ def home(request):
         cursor.execute(f"""INSERT INTO public."ToDoTasks"( title, description) VALUES ('{title}', '{description}');""")
         connection.commit()
         print(data_all)
-        title = None
-        description = None
-        return render(request, "home.html", {
-            "data": data,
-        })
 
-    else:
-        title = None
-        description = None
-        return render(request, "home.html", {
-            "data": data,
-        })
+    cursor.execute("""SELECT id, title, description FROM public."ToDoTasks";""")
+    data_sql = cursor.fetchall()
+    data = []
+
+    for i in data_sql:
+        data.append({"title": i[1], "description": i[2]})
+    return render(request, "home.html", {
+        "data": data,
+    })
