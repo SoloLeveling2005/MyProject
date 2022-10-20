ffc99daea433d42367a50d379155e5d210c94e76
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2022-10-20 22:54:13 +0600

    20.10.2022

--- Telegram_bots/SoloLeveling/Sung_Jinwoo.py
+++ Telegram_bots/SoloLeveling/Sung_Jinwoo.py
@@ -1,12 +1,11 @@
 import telebot
 
-bot = telebot.TeleBot("5713410226:AAGYZ1g9P0X9kEPS5H5wd876-iJXRMksbo8", parse_mode=None)
+bot = telebot.TeleBot("5488566542:AAEGQsiDrnLjwFCQb4kmbn7EJYnZqoaIfxk", parse_mode=None)
 
 print("Start")
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
-
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
